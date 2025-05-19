b35165f2d0317d06c2e93492a9fa7ab3c176a4f4
Sai Sindhu Rudraraju <saisindhu2311@gmail.com>
2025-05-18 22:38:01 -0500

    here we got a listbox with a scrollbar but side by side.

---
+++ tkinterListbox.py
@@ -0,0 +1,27 @@
+import tkinter
+import tkinter.messagebox as messagebox
+from tkinter import Listbox, Scrollbar, Entry, Button, Tk, END
+
+form = Tk()
+form.geometry("300x200")
+
+listbox = Listbox(form)
+listbox.grid(row=0, column=0)
+listbox.config(width=20,height=6)
+listbox.insert(0,"C")
+listbox.insert(1,"C++")
+listbox.insert(2,"C#")
+listbox.insert(3,"Java")
+listbox.insert(4,"Python")
+listbox.insert(5,"Javascript")
+listbox.insert(6,"Ruby")
+listbox.insert(7,"PHP")
+
+scrollbar = Scrollbar(form)
+scrollbar.grid(row=0, column=1, sticky='ns')
+
+listbox.config(yscrollcommand=scrollbar.set)
+scrollbar.config(command=listbox.yview)
+
+
+form.mainloop()
