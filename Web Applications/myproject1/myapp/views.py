47652fe169b5000a4e000de86534a32e96166c8a
Sai Sindhu Rudraraju <saisindhu2311@gmail.com>
2025-06-05 14:17:03 -0500

    django

---
+++ Web Applications/myproject1/myapp/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import ProductForm, Product
+
+# Create your views here.
+def home(request):
+    return HttpResponse("Welcome to myproject1!!")
+
+def hello(request):
+    productList = Product.objects.all()
+
+    product_form = ProductForm()
+    return render(request, 'hello.html', {'message': 'Hello, world!', 'product_form':product_form, 'productList': productList})
+
+def SaveProduct(request):
+    if request.method == 'POST':
+        form = ProductForm(request.POST, request.FILES)
+        print(form)
+        print(form.errors)
+        if form.is_valid():
+            form.save()
+            return HttpResponse("Product Saved Successfully!!")
+        else:
+            return HttpResponse("Error saving product.")
+    else:
+        return HttpResponse("Invalid request method.")
